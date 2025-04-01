1c9f1ea766af6064383ba85324bb38f78762e982
queolvi <korpachyovdmitriy@gmail.com>
2025-04-01 23:35:01 +0300

    py 1

---
+++ stepik/first.py
@@ -0,0 +1,11 @@
+
+print("Enter two numbers")
+a = int(input())
+b = int(input())
+print("if you wanna plus your values, enter 0, else, if you want sub - enter 1..")
+choice = bool(input())
+res = int(0)
+if choice(0): 
+    res  == a + b
+elif choice(1):
+ res == a - b
